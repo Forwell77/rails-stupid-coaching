75fa6a860baac2f48b5ad3f7dcc772cfc94bec01
Oussama El Boukioty <oelboukioty@gmail.com>
2023-08-14 17:19:13 +0100

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,14 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @question = params[:member]
-  end
+    @question = params[:users_question]
 
+    if @question == "I am going to work"
+      @answer = 'Great!'
+    elsif @question.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
